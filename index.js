e86cf78a8e49d60ca22c77201e884e418462930c
Mohit Virli <mvirli@cohesity.com>
2021-05-12 21:46:23 +0530

    Added polling stats & handled error more gracefully

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ const https = require('https');
 const pollIntervals = {};
 
 /**
+ * Statistics for polls.
+ */
+const pollStats = {};
+
+/**
+ * Saving old stats to show the diff, if any.
+ */
+const oldPollStats = {};
+
+/**
  * Login
  */
 bot.login(TOKEN);
@@ -45,7 +55,10 @@ bot.on('message', msg => {
             }, {
               name: 'clear',
               value: 'If you want to clear polling for the pin mentioned',
-            },
+            }, {
+              name: 'Currently Polling for',
+              value: Object.keys(pollIntervals).length ? Object.keys(pollIntervals).join(', ') : 'None',
+            }
           ],
         },
       });
@@ -63,57 +76,81 @@ bot.on('message', msg => {
       if (!first) console.log('Polling for', pincode);
       const pollingStarted = !!pollIntervals[pincode];
 
+      if (first && !pollingStarted && !pollStats[pincode]) {
+        pollStats[pincode] = {
+          success: 0,
+          fail: 0,
+        };
+        oldPollStats[pincode] = {...pollStats[pincode]};
+      }
+
       getCenters(params, res => {
-          const availabilityCheck = a => a.min_age_limit !== 45 && a.available_capacity !== 0;
-          const availableCenters = res.centers.filter(center => center.sessions.find(availabilityCheck));
+        const availabilityCheck = a => a.min_age_limit !== 45 && a.available_capacity !== 0;
+        const availableCenters = (res.centers || []).filter(center => center.sessions.find(availabilityCheck));
 
-          const embedMessage = {
-            color: 0x81b214,
-            fields: [],
-          };
+        const embedMessage = {
+          color: 0x81b214,
+          fields: [],
+        };
 
-          if (availableCenters.length === 0 && first) {
-            embedMessage.fields.push({
-              name: 'Not available',
-              value: 'Search karte raho lmao',
-            });
+        if (availableCenters.length === 0 && first) {
+          embedMessage.fields.push({
+            name: 'Not available',
+            value: 'Search karte raho lmao',
+          });
 
-            embedMessage.fields.push({
-              name: 'Polling Status',
-              value: pollingStarted ? 'Running' : 'Started',
-            });
+          embedMessage.fields.push({
+            name: 'Polling Status',
+            value: pollingStarted ? 'Running' : 'Started',
+          });
 
-            embedMessage.color = pollingStarted ? 0x00adb5 : 0xfb3640;
-            msg.channel.send({ embed: embedMessage });
-          } else if (availableCenters.length !== 0) {
-            msg.channel.send({ embed: {
-              title: 'GOTDAM VACCINE SLOTS AVAILABLE',
-              description: 'Start booking lmao, what you looking at?',
-              color: 0xf8f5f1,
-            }});
+          embedMessage.fields.push({
+            name: '✅ Polls',
+            value: `${pollStats[pincode].success} (+${pollStats[pincode].success - oldPollStats[pincode].success})`,
+            inline: true,
+          });
 
-            availableCenters.forEach(center => {
-              embedMessage.fields = [{
-                name: center.name,
-                value: `${center.state_name}, Fee Type - ${center.fee_type}`,
-              }];
+          embedMessage.fields.push({
+            name: '❌ Polls',
+            value: `${pollStats[pincode].fail} (+${pollStats[pincode].fail - oldPollStats[pincode].fail})`,
+            inline: true,
+          });
 
-              center.sessions
-                .filter(availabilityCheck)
-                .forEach(ses => {
-                  embedMessage.fields.push({
-                    name: `${moment(ses.date).format('D MMM YYYY')} (${ses.available_capacity} Slots)`,
-                    value: ses.slots.join(', '),
-                    inline: true,
-                  });
-                });
-
-              msg.channel.send({ embed: embedMessage });
-              clearInterval(pollIntervals[pincode]);
-              pollIntervals[pincode] = null;
-            })
+          if (pollingStarted) {
+            oldPollStats[pincode] = {...pollStats[pincode]};
           }
-        });
+
+          embedMessage.color = pollingStarted ? 0x00adb5 : 0xfb3640;
+          msg.channel.send({ embed: embedMessage });
+        } else if (availableCenters.length !== 0) {
+          msg.channel.send({ embed: {
+            title: 'GOTDAM VACCINE SLOTS AVAILABLE',
+            description: 'Start booking lmao, what you looking at?',
+            color: 0xf8f5f1,
+          }});
+
+          availableCenters.forEach(center => {
+            embedMessage.fields = [{
+              name: center.name,
+              value: `${center.state_name}, Fee Type - ${center.fee_type}`,
+            }];
+
+            center.sessions
+              .filter(availabilityCheck)
+              .forEach(ses => {
+                embedMessage.fields.push({
+                  name: `${moment(ses.date).format('D MMM YYYY')} (${ses.available_capacity} Slots)`,
+                  value: ses.slots.join(', '),
+                  inline: true,
+                });
+              });
+
+            msg.channel.send({ embed: embedMessage });
+            clearInterval(pollIntervals[pincode]);
+            pollIntervals[pincode] = null;
+          })
+        }
+      });
     }
 
     if (pollClear !== 'clear') {
@@ -173,6 +210,7 @@ function getCenters(params, callback) {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
     }
   };
 
@@ -184,7 +222,14 @@ function getCenters(params, callback) {
     res.on('data', res => response = res);
 
     res.on('end', d => {
-      callback(JSON.parse(response));
+      if (res.statusCode != 200) {
+        pollStats[params.pincode].fail++;
+        console.log('Error', res.statusCode);
+        callback({}, true);
+      } else {
+        pollStats[params.pincode].success++;
+        callback(JSON.parse(response));
+      }
     })
   });
 
